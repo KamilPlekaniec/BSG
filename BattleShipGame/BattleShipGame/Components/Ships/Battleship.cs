e1e21d1293b20cd06dab5da1156f7d80eb56aca7
Kamil <kp054467@student.ath.edu.pl>
2021-08-27 15:37:55 +0200

    added Battleship class

---
+++ BattleShipGame/BattleShipGame/Components/Ships/Battleship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShipGame.Components.Ships
+{
+    public class Battleship : Ship
+    {
+        public Battleship()
+        {
+            Name = "Okręt wojenny";
+            Width = 4;
+            FieldsType = FieldsType.Battleship;
+        }
+    }
+}
