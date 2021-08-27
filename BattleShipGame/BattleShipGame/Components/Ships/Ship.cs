18eea688d1bec0c76ef3c833819603328fa9e056
Kamil <kp054467@student.ath.edu.pl>
2021-08-27 15:21:14 +0200

    added abstract class which contain Ship details

---
+++ BattleShipGame/BattleShipGame/Components/Ships/Ship.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BattleShipGame.Components.Ships
+{
+    public enum FieldsType
+    {
+        [Description("*")]
+        Carrier,
+        [Description("&")]
+        Battleship,
+        [Description("@")]
+        Destroyer,
+        [Description("$")]
+        Submarine,
+        [Description("#")]
+        PatrolBoat,
+        [Description(" ")]
+        Empty,
+        [Description("+")]
+        Hit,
+        [Description("-")]
+        Miss
+    }
+    public class Ship
+    {
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Hits { get; set; }
+        public FieldsType FieldsType { get; set; }
+        public bool IsSunk
+        {
+            get
+            {
+                return Hits >= Width;
+            }
+        }
+    }
+}
