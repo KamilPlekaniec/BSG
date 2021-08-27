887670e31098be95a91659c599261588b4740276
Kamil <kp054467@student.ath.edu.pl>
2021-08-27 16:24:07 +0200

    update Game class, added SingleRound method

--- BattleShipGame/BattleShipGame/Components/Game.cs
+++ BattleShipGame/BattleShipGame/Components/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace BattleShipGame.Components
 {
@@ -22,6 +23,23 @@ namespace BattleShipGame.Components
 
             Player1.OutputBoards();
             Player2.OutputBoards();
+        }
+
+        public void SingleRound()
+        {
+            Console.Clear();
+            var coordinates = Player1.FireShot();
+            var result = Player2.ProcessShot(coordinates);
+            Player1.ProcessShotResult(coordinates, result);
+
+            if (!Player2.HasLost)
+            {
+                coordinates = Player2.FireShot();
+                result = Player1.ProcessShot(coordinates);
+                Player2.ProcessShotResult(coordinates, result);
+            }
+            Player1.OutputBoards();
+            Thread.Sleep(1500);
         }
     }
 }
