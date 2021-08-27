a6d80197430b30cb00d15706a5826753730ff1bd
Kamil <kp054467@student.ath.edu.pl>
2021-08-27 15:40:06 +0200

    added Destroyer class

---
+++ BattleShipGame/BattleShipGame/Components/Ships/Destroyer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShipGame.Components.Ships
+{
+    public class Destroyer : Ship
+    {
+        public Destroyer()
+        {
+            Name = "Niszczyciel";
+            Width = 3;
+            FieldsType = FieldsType.Destroyer;
+        }
+    }
+}
