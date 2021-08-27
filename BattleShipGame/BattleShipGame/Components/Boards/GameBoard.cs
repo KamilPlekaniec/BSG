7e886cfebd17a0d0136c6939518364be89fb195a
Kamil <kp054467@student.ath.edu.pl>
2021-08-27 15:43:08 +0200

    added GameBoard class

---
+++ BattleShipGame/BattleShipGame/Components/Boards/GameBoard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShipGame.Components.Boards
+{
+    public class GameBoard
+    {
+        public List<Field> Fields { get; set; }
+        public GameBoard()
+        {
+            Fields = new List<Field>();
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Fields.Add(new Field(i, j));
+                }
+            }
+        }
+    }
+}
