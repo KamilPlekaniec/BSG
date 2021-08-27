061d582f336121c2b0b493180561d6637ed33b77
Kamil <kp054467@student.ath.edu.pl>
2021-08-27 15:39:00 +0200

    added Carrer class

---
+++ BattleShipGame/BattleShipGame/Components/Ships/Carrier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShipGame.Components.Ships
+{
+    public class Carrier : Ship
+    {
+        public Carrier()
+        {
+            Name = "Lotniskowiec";
+            Width = 5;
+            FieldsType = FieldsType.Carrier;
+        }
+    }
+}
